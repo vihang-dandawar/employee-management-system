3a7f9649d8744a2f0ff109349402e5d422ffe610
Vihang <vihangreddy1234@gmail.com>
2025-06-30 09:00:54 +0530

    Full stack employee mgmt more features will be added soon

--- src/main/java/com/fullStack/fullStack/controller/EmployeeController.java
+++ src/main/java/com/fullStack/fullStack/controller/EmployeeController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-@CrossOrigin("*")
+
 @AllArgsConstructor
 @RequestMapping("/api/employees")
 @RestController
