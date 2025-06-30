7be3e2c67a40b0513f32bf81b53839191bbfad72
Vihang <vihangreddy1234@gmail.com>
2025-06-30 08:28:01 +0530

    project commited

--- src/main/java/com/fullStack/fullStack/Repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.fullStack.fullStack.Repository;
-
-import com.fullStack.fullStack.entity.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer> {
-
-
-}
